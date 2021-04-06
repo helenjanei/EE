94d75d667857aa6aa3f864767e58018a7e2a5306
helenjanei <helenjanei@gmail.com>
2021-04-06 15:19:59 +0100

    completes 2 hr window'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Information from './Assets/houses.json';
-
+import axios from 'axios'
 class App extends Component {
 
   constructor(){
     super();
 
     this.state={
-      search:null
+      search:null,
+      degreeDays:'',
+      designRegion: ''
     };
   }
 
@@ -18,7 +20,20 @@ class App extends Component {
     this.setState({search:keyword})
   }
 
+  setLocation=(data)=>{
+    let designRegion = data.location;
+    this.setState({location:designRegion})
+  }
 
+  getDegreeDays=(location)=>{
+     const headers = {
+            'x-api-key': 'f661f74e-20a7-4e9f-acfc-041cfb846505',
+            
+        };
+        axios.get('https://063qqrtqth.execute-api.eu-west-2.amazonaws.com/v1/weather?location={location}', { headers })
+            .then(response => this.setState({ degreeDays: response.location.degreeDays }));
+    }
+  
   
   render(){
   
